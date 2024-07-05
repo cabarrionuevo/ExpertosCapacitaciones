187d649f90bf694635708c67d0aa3608883f9f98
charly <cbarrionuevo@fidelatam.com>
2024-07-05 18:17:58 -0300

    Ajustes en función getLastUrl

--- js/aux.js
+++ js/aux.js
@@ -10,13 +10,20 @@ const logUrl = (event) => {
 }
 
 const getLastUrl = () => {    
-    let lastUrl = localStorage.getItem('url-data');    
+    let width = localStorage.getItem('current-width');
+		
+		if(!width){
+			localStorage.setItem('current-width', window.innerWidth);
+		}else{
+			if(window.innerWidth != width){
+				//la recarga del index se dio por una rotación de la pantalla
+				let lastUrl = localStorage.getItem('url-data');
 
-    if (lastUrl) {
-        const { href } = JSON.parse(lastUrl);
-        const urlActual = window.location.href;
-        localStorage.setItem('url-actual', JSON.stringify({urlActual}));        
-        window.location.assign(href);
-        localStorage.removeItem('url-data');
-    }
+				if(lastUrl){
+					const { href } = JSON.parse(lastUrl);
+					window.location.assign(href);
+					localStorage.setItem('current-width', window.innerWidth);					
+				}
+			}
+		}		
 }
